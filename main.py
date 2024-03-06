2440dc0425c665d7d5009638fa55cde18461328c
Patrick D <patrick.montejo@gmail.com>
2024-03-06 18:59:14 -0500

    cleanup code

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 import pandas as pd
 
-# for (key, value) in student_dict.items():
-
-# for (index, row) in student_data_frame.iterrows():
-# Access index and row
-# Access row.student or row.score
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-# TODO 1. Create a dictionary in this format:
 nato_phonetic_df = pd.read_csv("nato_phonetic_alphabet.csv")
 nato_phonetic_list = {row.letter: row.code for (index, row) in nato_phonetic_df.iterrows()}
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 word = input("Type in a name: ").upper()
 
 phonetic_name = [nato_phonetic_list[letter] for letter in word]
