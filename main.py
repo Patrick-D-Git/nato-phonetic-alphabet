70dadcc37ec7b3eec0638f82791d2d6e47c3f7b7
Patrick D <patrick.montejo@gmail.com>
2024-03-06 19:00:54 -0500

    changed a few variable to make the code more readable

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-nato_phonetic_df = pd.read_csv("nato_phonetic_alphabet.csv")
-nato_phonetic_list = {row.letter: row.code for (index, row) in nato_phonetic_df.iterrows()}
+data = pd.read_csv("nato_phonetic_alphabet.csv")
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 word = input("Type in a name: ").upper()
 
-phonetic_name = [nato_phonetic_list[letter] for letter in word]
+phonetic_name = [phonetic_dict[letter] for letter in word]
 
 print(phonetic_name)
